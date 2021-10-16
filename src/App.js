49a0d09954f4839ee62a9b55826736892d3cca74
Mykhailo Madzhara <2madzhara@gmail.com>
2021-10-16 19:32:50 +0300

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Some Header</header>
+    </div>
+  );
+}
+
+export default App;
